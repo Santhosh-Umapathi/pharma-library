e5fc6eb75c8ed665bd911bf78958824beae23a36
Santhosh Umapathi <santhosh.professional1@gmail.com>
2025-04-25 16:47:13 +0200

    feat: add TopTabs component for tab navigation

---
+++ src/components/Tabs/TopTabs/TopTabs.tsx
@@ -0,0 +1,16 @@
+import { t } from "@/localization";
+import { TopTab } from "@/components/Tabs/TopTab";
+import { TSelectedTab } from "@/store/library/types";
+
+export const TopTabs = () => {
+  return (
+    <div
+      id="top-tabs"
+      className="flex items-center w-full mt-10 bg-bgSecondary rounded-lg"
+    >
+      {t.tabs.map(({ id, label }) => (
+        <TopTab key={id} {...{ id: id as TSelectedTab, label }} />
+      ))}
+    </div>
+  );
+};

f83df4de835ed0ad0d011a99b025bd7b3623a07c
Santhosh Umapathi <santhosh.professional1@gmail.com>
2025-04-25 23:12:28 +0200

    feat: refactor Home component to initialize library data and include AssetSection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,16 +2,22 @@ import { HeaderSection } from "@/components/HeaderSection";
 import { SearchBar } from "@/components/SearchBar";
 import { TopTabs } from "@/components/Tabs/TopTabs";
 import { t } from "@/localization";
+import { AssetSection } from "@/components/AssetsSection";
+import { useLibraryStore } from "@/store";
 
-const Home = () => {
+const Home = async () => {
+  const setInitialData = useLibraryStore.getState().setInitialData;
+  setInitialData();
+
   return (
-    <div className="flex bg-bgPrimary w-screen flex-col items-center px-60 py-10 h-screen">
+    <div className="flex bg-bgPrimary w-screen flex-col items-center px-60 py-10">
       <HeaderSection
         title={t.header.title}
         description={t.header.description}
       />
       <SearchBar />
       <TopTabs />
+      <AssetSection />
     </div>
   );
 };

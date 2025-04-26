a96fdae0b21d2df6beee2fcf3578d8693a8ee269
Santhosh Umapathi <santhosh.professional1@gmail.com>
2025-04-26 22:47:27 +0200

    feat: refactor search handling to save recent searches and improve clear functionality

--- src/components/SearchBar/controller.ts
+++ src/components/SearchBar/controller.ts
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useRef, useState } from "react";
+import { useCallback, useEffect, useMemo, useRef, useState } from "react";
 import debounce from "lodash.debounce";
 import { postSearchAssets } from "@/api/search.api";
 import { useLibraryStore } from "@/store";
@@ -11,10 +11,10 @@ export const useController = () => {
   const isClearIconVisible = searchText.length > 0;
 
   // Clear search results from the store
-  const clearStoreSearchResults = () => {
+  const clearStoreSearchResults = useCallback(() => {
     const setSearchResults = useLibraryStore.getState().setSearchResults;
     setSearchResults([]);
-  };
+  }, []);
 
   // Clear search input
   const clearSearch = () => {
@@ -22,6 +22,36 @@ export const useController = () => {
 
     // Clear stored search results
     clearStoreSearchResults();
+  };
+
+  // Save recent searches to local storage
+  const saveRecentSearches = (search: string) => {
+    const recentSearches = localStorage.getItem("recentSearches");
+
+    // Create a new search object
+    if (!recentSearches) {
+      localStorage.setItem(
+        "recentSearches",
+        JSON.stringify([{ id: Date.now(), search }])
+      );
+      return;
+    }
+
+    // Update/Modify existing searches
+    const recentSearchesArray = JSON.parse(recentSearches);
+
+    // Check if search already exists
+    const searchExists = recentSearchesArray.some(
+      (item: { search: string }) => item.search === search
+    );
+
+    // Only add the search if it doesn't already exist
+    if (!searchExists) {
+      recentSearchesArray.push({ id: Date.now(), search });
+    }
+
+    // Save updated recent searches to local storage
+    localStorage.setItem("recentSearches", JSON.stringify(recentSearchesArray));
   };
 
   // Debounce the search function to limit the number of API calls
@@ -42,20 +72,14 @@ export const useController = () => {
         // Sanitize search text - Remove leading and trailing spaces, convert to lowercase
         const sanitizedSearchText = value.trim().toLowerCase();
         const setSearchResults = useLibraryStore.getState().setSearchResults;
-
+        // Save recent searches to local storage
+        saveRecentSearches(sanitizedSearchText);
+        // Fetch search results from the API
         const results = await postSearchAssets(sanitizedSearchText);
         setSearchResults(results.data);
       }, 500),
     []
   );
-
-  // Handle input change
-  const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const value = event.target.value;
-    setSearchText(value);
-    // Search for assets
-    debouncedSearch(value);
-  };
 
   // Refetch data on focus if search text is not empty
   const onFocus = () => {
@@ -65,9 +89,16 @@ export const useController = () => {
     }
   };
 
-  // Handle click outside of the search results and input
-  useEffect(() => {
-    const handleClickOutside = (event: MouseEvent) => {
+  // Handle input change
+  const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const value = event.target.value;
+    setSearchText(value);
+    // Search for assets
+    debouncedSearch(value);
+  };
+
+  const handleClickOutside = useCallback(
+    (event: MouseEvent) => {
       if (
         searchResultsRef.current &&
         !searchResultsRef.current.contains(event.target as Node) &&
@@ -77,8 +108,12 @@ export const useController = () => {
         // Clear search results if clicked outside
         clearStoreSearchResults();
       }
-    };
+    },
+    [clearStoreSearchResults]
+  );
 
+  // Handle click outside of the search results and input
+  useEffect(() => {
     // Add event listener
     document.addEventListener("mousedown", handleClickOutside);
 
@@ -86,7 +121,7 @@ export const useController = () => {
     return () => {
       document.removeEventListener("mousedown", handleClickOutside);
     };
-  }, []);
+  }, [handleClickOutside]);
 
   return {
     searchText,

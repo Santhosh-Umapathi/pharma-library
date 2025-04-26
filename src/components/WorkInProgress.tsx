e3bc560548689a5c8a75f5cd70983d215b043ad9
Santhosh Umapathi <santhosh.professional1@gmail.com>
2025-04-26 06:19:59 +0200

    fix: adjust layout of WorkInProgress component for improved styling

--- src/components/WorkInProgress.tsx
+++ src/components/WorkInProgress.tsx
@@ -1,8 +1,8 @@
 import { t } from "@/localization";
 
 export const WorkInProgress = () => (
-  <div className="flex flex-col items-center justify-center w-full">
-    <span className="text-2xl font-bold my-40">{t.workInProgress.title}</span>
+  <div className="flex flex-col items-center justify-center w-full my-40">
+    <span className="text-2xl font-bold">{t.workInProgress.title}</span>
     <span className="text-lg mt-8">{t.workInProgress.description}</span>
   </div>
 );

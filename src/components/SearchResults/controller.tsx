d2be63a19008e8b65bd4d5ffd24563d164f04bcf
Santhosh Umapathi <santhosh.professional1@gmail.com>
2025-04-27 19:50:57 +0200

    refactor: remove RecentSearch and SearchBar components along with their controllers and types

--- src/components/SearchResults/controller.tsx
+++
@@ -1,9 +0,0 @@
-import { useLibraryStore } from "@/store";
-
-export const useController = () => {
-  const searchResults = useLibraryStore((state) => state.searchResults);
-
-  const isSearchResultsEmpty = searchResults.length === 0;
-
-  return { searchResults, isSearchResultsEmpty };
-};
--- src/components/SearchResults/types.ts
+++
@@ -1,3 +0,0 @@
-export type TProps = {
-  searchResultsRef: React.RefObject<HTMLDivElement | null>;
-};

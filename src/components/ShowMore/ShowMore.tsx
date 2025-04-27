15f54718bafa44dcbc905c896bb5dbbea95daaaa
Santhosh Umapathi <santhosh.professional1@gmail.com>
2025-04-27 21:50:49 +0200

    refactor: improve layout and spacing in ShowMore component for better responsiveness

--- src/components/ShowMore/ShowMore.tsx
+++ src/components/ShowMore/ShowMore.tsx
@@ -6,13 +6,19 @@ export const ShowMore = () => {
 
   return (
     <div
-      className="flex flex-col items-center justify-center w-full p-8 mt-10"
+      className="flex flex-col items-center justify-center w-full 
+      p-4 mt-5
+      md:p-8 md:mt-10
+      "
       id="show-more"
     >
       {!showMore ? (
         // Show the button if showMore is false
         <button
-          className="bg-buttonSecondary text-white p-4 rounded-lg hover:opacity-70 transition-opacity duration-300 cursor-pointer"
+          className="bg-buttonSecondary text-white rounded-lg hover:opacity-70 transition-opacity duration-300 cursor-pointer
+          p-3 text-sm
+          md:p-4 md:text-base
+          "
           type="button"
           onClick={handleShowMore}
         >
@@ -21,11 +27,21 @@ export const ShowMore = () => {
       ) : (
         // Show the message if showMore is true - Mocking end of results
         <div
-          className="flex items-center justify-center cursor-pointer hover:opacity-70 transition-opacity duration-300"
+          className="flex items-center justify-center cursor-pointer hover:opacity-70 transition-opacity duration-300
+          text-sm
+          md:text-base
+          "
           onClick={scrollToTop}
         >
           <span>{t.showMore.notSeeing}</span>
-          <span className="text-xl font-bold animate-bounce ml-4">↑</span>
+          <span
+            className="font-bold animate-bounce
+          text-lg ml-2
+          md:text-xl md:ml-4
+          "
+          >
+            ↑
+          </span>
         </div>
       )}
     </div>
